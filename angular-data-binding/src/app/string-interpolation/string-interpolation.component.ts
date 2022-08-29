be2abcea7af818c0a9572747743d028a34a75656
Sankha Subhra Mondal <sankha123@gmail.com>
2022-08-29 21:26:15 +0530

    Angular Data-Binding

---
+++ angular-data-binding/src/app/string-interpolation/string-interpolation.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-string-interpolation',
+  templateUrl: './string-interpolation.component.html',
+  styleUrls: ['./string-interpolation.component.css']
+})
+export class StringInterpolationComponent implements OnInit {
+
+  name: string = "Raj Deep";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  display(): String {
+    return "Welcome to Angular";
+  }
+  sayHello(name: string): String {
+    return "WelCome "+name;
+  }
+
+}
